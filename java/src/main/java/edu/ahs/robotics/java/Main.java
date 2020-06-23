65f47c45f1c60ca56feb83fdfcaf0e6ee618e331
afleissner <67342756+afleissner@users.noreply.github.com>
2020-06-23 16:59:35 -0400

    Added my name to the "Hello World!" message.

--- java/src/main/java/edu/ahs/robotics/java/Main.java
+++ java/src/main/java/edu/ahs/robotics/java/Main.java
@@ -1,7 +1,8 @@
 package edu.ahs.robotics.java;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! My name is Boris!");
     }
 }
